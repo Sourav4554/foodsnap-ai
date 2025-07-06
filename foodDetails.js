af73c43976ecff9ab9c2a53cac2ec34b796510f8
Sourav krishna <142560714+Sourav4554@users.noreply.github.com>
2025-07-06 07:34:57 +0530

    Create foodDetails.js
    create a json file for food details

---
+++ foodDetails.js
@@ -0,0 +1,101 @@
+[
+  {
+    "name": "chappathi",
+    "calories": 187,
+    "fat": 6.2,
+    "protein": 6.7,
+    "carbs": 28.2,
+    "fiber": 4.9,
+    "sugar": 0.5
+  },
+  {
+    "name": "chicken recipe",
+    "calories": 165,
+    "fat": 3.6,
+    "protein": 31,
+    "carbs": 0,
+    "fiber": 0,
+    "sugar": 0
+  },
+  {
+    "name": "chicken biriyani",
+    "calories": 220,
+    "fat": 8.3,
+    "protein": 12.1,
+    "carbs": 22.3,
+    "fiber": 1.5,
+    "sugar": 1.8
+  },
+  {
+    "name": "egg recipie",
+    "calories": 143,
+    "fat": 9.5,
+    "protein": 12.6,
+    "carbs": 1.1,
+    "fiber": 0,
+    "sugar": 1.1
+  },
+  {
+    "name": "idili",
+    "calories": 146,
+    "fat": 0.4,
+    "protein": 4.1,
+    "carbs": 30.4,
+    "fiber": 1.3,
+    "sugar": 0.2
+  },
+  {
+    "name": "idiyappam",
+    "calories": 153,
+    "fat": 0.3,
+    "protein": 2.6,
+    "carbs": 34.3,
+    "fiber": 1.1,
+    "sugar": 0.4
+  },
+  {
+    "name": "kadala curry",
+    "calories": 154,
+    "fat": 6.7,
+    "protein": 5.9,
+    "carbs": 17.5,
+    "fiber": 4.1,
+    "sugar": 1.4
+  },
+  {
+    "name": "payassam",
+    "calories": 200,
+    "fat": 6.7,
+    "protein": 3.5,
+    "carbs": 32.4,
+    "fiber": 0.8,
+    "sugar": 18.6
+  },
+  {
+    "name": "Porotta",
+    "calories": 312,
+    "fat": 15.5,
+    "protein": 5.2,
+    "carbs": 38.5,
+    "fiber": 2.4,
+    "sugar": 1.6
+  },
+  {
+    "name": "puttu",
+    "calories": 190,
+    "fat": 4.5,
+    "protein": 4.2,
+    "carbs": 36.5,
+    "fiber": 2.7,
+    "sugar": 1.8
+  },
+  {
+    "name": "sadya",
+    "calories": 350,
+    "fat": 12.3,
+    "protein": 6.5,
+    "carbs": 45.8,
+    "fiber": 5.1,
+    "sugar": 4.3
+  }
+]
